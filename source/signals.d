b86a4097fc2a8d66f32a1aeaee788829c28abacb
Jonathan Balls <jonathanballs@protonmail.com>
2020-01-09 19:08:39 +0000

    Add signals module - stolen from logmaster :p

---
+++ source/signals.d
@@ -0,0 +1,33 @@
+module signals;
+import std.stdio;
+
+template Signal(T...) {
+
+    // Type of a slot
+    alias slot_t = void delegate(T);
+
+    class Signal {
+        private slot_t[] slots;
+
+        void emit(T...)(T t) {
+            foreach(f; this.slots) {
+                f(t);
+            }
+        }
+
+        void connect(slot_t slot) {
+            slots ~= slot;
+        }
+    }
+}
+
+unittest {
+    writeln("Testing signals...");
+
+    bool called;
+    void handler(int i, string s) { called = true; }
+    auto s = new Signal!(int, string);
+    s.connect(&handler);
+    s.emit(0x3, "string");
+    assert(called);
+}
