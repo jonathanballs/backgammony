fff45edfdb208ba747d0165e45b17a91eb36c904
Jonathan Balls <jonathanballs@protonmail.com>
2020-01-14 00:33:21 +0000

    Fetch evaluation from gnubg

--- source/ai/gnubg.d
+++ source/ai/gnubg.d
@@ -1,5 +1,17 @@
 module ai.gnubg;
 
+import std.algorithm;
+import std.array;
+import std.conv;
+import std.datetime;
+import std.format;
+import std.process;
+import std.stdio;
+import std.string;
+
+import formats.fibs;
+import game;
+
 /**
  * Manages interfacing with GnuBackgammon
  */
@@ -17,6 +29,17 @@ struct GnubgEvalContext {
 }
 
 /**
+ * Response of a gnubg position evaluation
+ */
+struct GnubgEvalResult {
+    float chanceWin;            /// Chance of winning
+    float chanceWinGammon;      /// Chance of winning by gammon
+    float chanceWinBackGammon;  /// Chance of winning by backgammon
+    float chanceLoseGammon;     /// Chance of losing by gammon
+    float chanceLoseBackGammon; /// Chance of losing by backgammon
+}
+
+/**
  * Default contexts lifted from gnubg source code.
  * https://cvs.savannah.gnu.org/viewvc/gnubg/gnubg/eval.c?revision=1.485&view=markup#l316
  */
@@ -31,3 +54,37 @@ GnubgEvalContext[] gnubgDefaultEvalContexts = [
     { "Gandmaster",     true, 3, true,  true, 0.0f },
     { "4-ply",          true, 4, true,  true, 0.0f },
 ];
+
+/**
+ * Request an evaluation from gnubg
+ */
+GnubgEvalResult evaluatePosition(GameState gs, GnubgEvalContext context) {
+    assert(gs.turnState == TurnState.MoveSelection);
+    string gnubgCommand = format!"eval FIBSBOARD %s PLIES 0 CUBEFUL\n"(gs.toFibsString());
+
+    // Create commands
+    string tmpFileName = "/tmp/gnubg-" ~ Clock.currTime().toISOString() ~ "";
+    auto f = File(tmpFileName, "w");
+    f.write(gnubgCommand);
+    f.close();
+
+    // Run 
+    auto p = execute(["gnubg", "--tty", "-c", tmpFileName]);
+
+    // Filter output to 0-ply static line
+    string[] r = p.output.split('\n').filter!(s => s.indexOf("static: ") > -1).array;
+    if (!r.length) throw new Exception("Failed to get results from gnubg");
+    float[] results = r[0].split().array[1..$].map!(n => n.chomp.to!float).array;
+    if (results.length < 6) throw new Exception("Failed to get results from gnubg");
+
+    return GnubgEvalResult(results[0], results[1], results[2], results[3], results[4]);
+}
+
+unittest {
+    writeln("Testing gnubg...");
+    auto gs = new GameState();
+    gs.newGame();
+    gs.rollDice(3, 3);
+    auto eval = evaluatePosition(gs, gnubgDefaultEvalContexts[2]);
+    writeln(eval);
+}
