6da136fdda9067690890ec3441994ffdf2c5894d
Jonathan Balls <jonathanballs@protonmail.com>
2020-02-04 23:49:04 +0000

    Further mac packaging progress

--- resources/launcher.sh
+++ resources/launcher.sh
@@ -27,6 +27,7 @@ export XDG_DATA_DIRS="$bundle_data"
 export GTK_DATA_PREFIX="$bundle_res"
 export GTK_EXE_PREFIX="$bundle_res"
 export GTK_PATH="$bundle_res"
+export GTK_THEME=Adwaita:dark
 
 # PANGO_* is no longer needed for pango >= 1.38
 
@@ -34,7 +35,7 @@ export PANGO_RC_FILE="$bundle_etc/pango/pangorc"
 export PANGO_SYSCONFDIR="$bundle_etc"
 export PANGO_LIBDIR="$bundle_lib"
 
-export GDK_PIXBUF_MODULE_FILE="$bundle_lib/gdk-pixbuf-2.0/2.10.0/loaders.cache"
+# export GDK_PIXBUF_MODULE_FILE="$bundle_lib/gdk-pixbuf-2.0/2.10.0/loaders.cache"
 if [ `uname -r | cut -d . -f 1` -ge 10 ]; then
     export GTK_IM_MODULE_FILE="$bundle_etc/gtk-3.0/gtk.immodules"
 fi
