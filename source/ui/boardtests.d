461ee0b8ec7c927b5fc7c57a1596edefc5c493fa
Jonathan Balls <jonathanballs@protonmail.com>
2020-02-09 18:11:47 +0000

    noMovesMessageAI ui test

--- source/ui/boardtests.d
+++ source/ui/boardtests.d
@@ -9,6 +9,7 @@ import gtk.Box;
 import ui.window;
 import ui.boardWidget;
 import game;
+import player;
 
 // UDA for uitests
 private enum uitest;
@@ -65,6 +66,43 @@ private class UITests {
         gs.points[23] = Point(Player.P2, 2);
         gs.points[24] = Point(Player.P2, 2);
 
+        gs.points[1] = Point(Player.P1, 2);
+        gs.points[2] = Point(Player.P1, 2);
+        gs.points[3] = Point(Player.P1, 2);
+        gs.points[4] = Point(Player.P1, 2);
+        gs.points[5] = Point(Player.P1, 2);
+        gs.points[6] = Point(Player.P1, 2);
+
         gs.takenPieces[Player.P1] = 1;
+        gs.takenPieces[Player.P2] = 1;
+    }
+
+    @uitest static void noMovesMessageAI(BackgammonWindow w) {
+        import std.variant : Variant;
+        import ai.gnubg;
+        Variant aiConfig = gnubgDefaultEvalContexts[0];
+
+        auto gs = new GameState(
+            PlayerMeta("Player 1", "gnubg", PlayerType.AI, aiConfig),
+            PlayerMeta("Player 2", "gnubg", PlayerType.AI, aiConfig)
+        );
+        w.setGameState(gs);
+        gs.newGame();
+        gs.points[19] = Point(Player.P2, 2);
+        gs.points[20] = Point(Player.P2, 2);
+        gs.points[21] = Point(Player.P2, 2);
+        gs.points[22] = Point(Player.P2, 2);
+        gs.points[23] = Point(Player.P2, 2);
+        gs.points[24] = Point(Player.P2, 2);
+
+        gs.points[1] = Point(Player.P1, 2);
+        gs.points[2] = Point(Player.P1, 2);
+        gs.points[3] = Point(Player.P1, 2);
+        gs.points[4] = Point(Player.P1, 2);
+        gs.points[5] = Point(Player.P1, 2);
+        gs.points[6] = Point(Player.P1, 2);
+
+        gs.takenPieces[Player.P1] = 1;
+        gs.takenPieces[Player.P2] = 1;
     }
 }
