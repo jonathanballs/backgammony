144238b68f02b393666f958497474b205343d65c
Jonathan Balls <jonathanballs@protonmail.com>
2020-02-16 13:24:23 +0000

    Further flatpak development

--- dmd-configure.sh
+++ dmd-configure.sh
@@ -6,7 +6,7 @@ all:
 
 install:
 	mkdir -p /app/bin
-	cp -r src/dmd /app/bin
+	cp generated/linux/release/64/dmd /app/bin
 	echo -e "[Environment64]" > /app/bin/dmd.conf
 	echo -e "DFLAGS=-I/app/src/phobos -I/app/src/druntime/import -L-L/app/linux/release/64" >> /app/bin/dmd.conf
 
