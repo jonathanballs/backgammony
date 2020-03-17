bd1870f378b917349477ef85b2169a00f2055c7c
Jonathan Balls <jonathanballs@protonmail.com>
2020-03-17 17:54:13 +0000

    Add docs and disconnect() and clear() member functions to Signal

--- source/utils/signals.d
+++ source/utils/signals.d
@@ -1,25 +1,52 @@
 module utils.signals;
 import std.stdio;
 
-// TODO:
-// - Disconnect logic...
-
+/**
+ * Signals and used for communication between objects. The user can create a
+ * signal on one of their objects which can be subscribed to by other parts of
+ * the application. This is modelled after QT signals.
+ */
 template Signal(T...) {
 
     // Type of a slot
     alias slot_t = void delegate(T);
 
     class Signal {
-        slot_t[] slots;
+        private slot_t[] slots;
 
+        /**
+         * Call each of the signal handlers in turn in the order that they were
+         * added.
+         */
         void emit(T...)(T t) {
-            foreach(f; this.slots) {
+            foreach (f; this.slots) {
                 f(t);
             }
         }
 
+        /**
+         * Connect a new callback to a signal
+         */
         void connect(slot_t slot) {
             slots ~= slot;
+        }
+
+        /**
+         * Disconnect a callback from a signal
+         */
+        void disconnect(slot_t slot) {
+            slot_t[] newSlots = [];
+            foreach (s; slots) {
+                if (s != slot) newSlots ~= s;
+            }
+            slots = newSlots;
+        }
+
+        /**
+         * Clear all callbacks
+         */
+        void clear() {
+            slots = [];
         }
     }
 }
@@ -27,10 +54,28 @@ template Signal(T...) {
 unittest {
     writeln("Testing Signals");
 
+    /**
+     * Test that signal can be called
+     */
     bool called;
     void handler(int i, string s) { called = true; }
     auto s = new Signal!(int, string);
     s.connect(&handler);
+    assert(s.slots.length == 1);
     s.emit(0x3, "string");
     assert(called);
+    
+    /**
+     * Test clearing
+     */
+    s.clear();
+    assert(s.slots.length == 0);
+
+    /**
+     * Test removing
+     */
+    s.connect(&handler);
+    assert(s.slots.length == 1);
+    s.disconnect(&handler);
+    assert(s.slots.length == 0);
 }
