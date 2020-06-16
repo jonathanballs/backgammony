6ba7b97de8a3a06c7d04cf1cff4499c7c693c86c
Jonathan Balls <jonathanballs@protonmail.com>
2020-06-16 23:37:25 +0100

    Fill MatchOverviewBox with skeleton info

--- source/ui/matchoverviewbox.d
+++ source/ui/matchoverviewbox.d
@@ -1,12 +1,14 @@
 module ui.matchoverviewbox;
 
 import gtk.Box;
+import gtk.Fixed;
 import gtk.Label;
 import gtk.StyleContext;
 import gtk.CssProvider;
 
 import gameplay.match;
 import gameplay.player;
+
 
 /**
  * Display an overview of the current match - typically in the main window above
@@ -16,20 +18,61 @@ class MatchOverviewBox : Box {
     private:
     BackgammonMatch match;
 
+    // Scorebox
+    Box scoreBox;
+    Label p1Score;
+    Label p2Score;
+    Label scoreBoxDash;
+
+    Box p1Info;
+    Label p1Name;
+
+    Box p2Info;
+    Label p2Name;
+
     public this() {
-        super(GtkOrientation.VERTICAL, 0);
-        this.add(new Label("MATCH INFORMATION"));
+        super(GtkOrientation.HORIZONTAL, 0);
         this.setSizeRequest(300, 50);
 
+        // Styling
         StyleContext styleContext = this.getStyleContext();      
         CssProvider cssProvider = new CssProvider();         
         cssProvider.loadFromData("box {"      
             ~ "padding: 10px;"
             ~ "border-bottom-width: 1px;"      
             ~ "border-bottom-style: solid;"      
-            ~ "border-color: #1b1b1b;"      
-            ~ "background-color: @theme_bg_color }");
+            ~ "border-color: @borders;"      
+            ~ "background-color: alpha(@borders , 0.55); }");
         styleContext.addProvider(cssProvider, STYLE_PROVIDER_PRIORITY_APPLICATION);    
+
+        // Score box
+        p1Score = new Label("0");
+        p1Score.setWidthChars(2);
+        // p1Score.setAlignment(1.0, 0.5);
+        p2Score = new Label("0");
+        p2Score.setWidthChars(2);
+        // p2Score.setAlignment(0.0, 0.5);
+        scoreBoxDash = new Label("-");
+        scoreBox = new Box(GtkOrientation.HORIZONTAL, 0);
+        scoreBox.packStart(p1Score, false, false, 0);
+        scoreBox.packStart(scoreBoxDash, false, false, 0);
+        scoreBox.packStart(p2Score, false, false, 0);
+        scoreBox.setHexpand(false);
+
+        p1Info = new Box(GtkOrientation.HORIZONTAL, 0);
+        p1Name = new Label("jonathanballs");
+        p1Name.setAlignment(1.0, 0.5);
+        p1Info.packEnd(p1Name, true, true, 0);
+        p1Info.setSizeRequest(200, 0);
+
+        p2Info = new Box(GtkOrientation.HORIZONTAL, 0);
+        p2Name = new Label("gnubg_intermediate");
+        p2Info.add(p2Name);
+        p2Info.setSizeRequest(200, 0);
+
+        this.packStart(p1Info, true, true, 0);
+        this.packStart(scoreBox, false, false, 0);
+        this.packStart(p2Info, true, true, 0);
     }
 
     public void setMatch(BackgammonMatch m) {
