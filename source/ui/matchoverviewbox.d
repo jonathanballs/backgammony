3ef28245f8e553f6e4ca8cb65b87aedb19e10100
Jonathan Balls <jonathanballs@protonmail.com>
2020-06-17 17:46:37 +0100

    Add 'no game in progress' message to match info box

--- source/ui/matchoverviewbox.d
+++ source/ui/matchoverviewbox.d
@@ -8,6 +8,7 @@ import gtk.Fixed;
 import gtk.Label;
 import gtk.StyleContext;
 import gtk.Widget;
+import gtk.Stack;
 
 import gameplay.match;
 import gameplay.player;
@@ -22,15 +23,20 @@ class MatchOverviewBox : Box {
     private:
     BackgammonMatch match;
 
-    // Scorebox
+    Stack displayStack;
+
+    // No match running
+    Box noMatchBox;
+    Label noMatchLabel;
+
+    // Match info
+    Box matchRunningBox;
     Box scoreBox;
     Label p1Score;
     Label p2Score;
     Label scoreBoxDash;
-
     Box p1Info;
     Label p1Name;
-
     Box p2Info;
     Label p2Name;
 
@@ -65,31 +71,48 @@ class MatchOverviewBox : Box {
         scoreBox.setHexpand(false);
 
         p1Info = new Box(GtkOrientation.HORIZONTAL, 0);
-        p1Name = new Label("jonathanballs");
+        p1Name = new Label("");
         p1Name.setAlignment(1.0, 0.5);
         p1Info.packEnd(p1Name, true, true, 0);
         p1Info.setSizeRequest(200, 0);
 
         p2Info = new Box(GtkOrientation.HORIZONTAL, 0);
-        p2Name = new Label("gnubg_intermediate");
+        p2Name = new Label("");
         p2Info.add(p2Name);
         p2Info.setSizeRequest(200, 0);
 
-        this.packStart(p1Info, true, true, 0);
-        this.packStart(scoreBox, false, false, 0);
-        this.packStart(p2Info, true, true, 0);
+        matchRunningBox = new Box(GtkOrientation.HORIZONTAL, 0);
+        matchRunningBox.packStart(p1Info, true, true, 0);
+        matchRunningBox.packStart(scoreBox, false, false, 0);
+        matchRunningBox.packStart(p2Info, true, true, 0);
+
+        noMatchBox = new Box(GtkOrientation.HORIZONTAL, 0);
+        noMatchLabel = new Label("No game in progress");
+        noMatchLabel.getStyleContext().addClass("dim-label");
+        noMatchBox.packStart(noMatchLabel, true, false, 0);
+
+        displayStack = new Stack();
+        displayStack.addNamed(matchRunningBox, "matchRunning");
+        displayStack.addNamed(noMatchBox, "noMatchRunning");
+
+        this.packStart(displayStack, true, true, 0);
     }
 
     public void setMatch(BackgammonMatch m) {
         this.match = m;
+        displayStack.setTransitionType(GtkStackTransitionType.CROSSFADE);
+        displayStack.setVisibleChild(matchRunningBox);
     }
 
     public bool onTick(Widget w, FrameClock f) {
         if (match) {
+            displayStack.setVisibleChild(matchRunningBox);
             p1Name.setText(match.player1.name);
             p2Name.setText(match.player2.name);
             p1Score.setText(match.p1score.to!string);
             p2Score.setText(match.p1score.to!string);
+        } else {
+            displayStack.setVisibleChild(noMatchBox);
         }
         return true;
     }
