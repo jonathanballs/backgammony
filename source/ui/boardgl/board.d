48e1042b5f4c196ff95a444b2c26996156f798cf
Jonathan Balls <jonathanballs@protonmail.com>
2021-03-10 23:00:17 +0000

    Move shader init and compilation code to separate module

--- source/ui/boardgl/board.d
+++ source/ui/boardgl/board.d
@@ -13,6 +13,8 @@ import gtk.GLArea;
 import gtk.Widget;
 
 import glcore;
+import ui.boardgl.shaders : initShaders;
+import gl3n.linalg;
 
 class BoardGL : GLArea {
 
@@ -38,6 +40,7 @@ public:
     GLuint m_Program;
     GLuint m_Mvp;
 
+    // Create resources for the display of the widget
     void realize(Widget) {
         makeCurrent();
         GLuint position_index;
@@ -47,6 +50,7 @@ public:
         initBuffers(position_index, color_index);
     }
 
+    // Destroy resources for the display of the widget
     void unrealize(Widget) {
         makeCurrent();
         glDeleteBuffers(1, &m_Vao);
@@ -76,12 +80,12 @@ public:
     }
 
     void drawTriangle() {
-        immutable mvp = getIdentityMatrix();
+        immutable mvp = mat4.identity;
 
         glUseProgram(m_Program);
 
         // update the "mvp" matrix we use in the shader
-        glUniformMatrix4fv(m_Mvp, 1, GL_FALSE, mvp.ptr);
+        glUniformMatrix4fv(m_Mvp, 1, GL_FALSE, mvp.value_ptr);
 
         glBindBuffer(GL_ARRAY_BUFFER, m_Vao);
         glEnableVertexAttribArray(0);
@@ -97,122 +101,25 @@ public:
     }
 
     void initBuffers(uint position_index, uint color_index) {
+        // Vertex data of the triangle.
         static immutable GLfloat[] vertex_data = [
-            0.0f, 0.5f, 0.0f, 1.0f, 0.5f,
-            -0.366f, 0.0f, 1.0f, -0.5f,
-            -0.366f, 0.0f, 1.0f,
+            0.0f, 0.5f, 0.0f, 1.0f,
+            0.5f, -0.366f, 0.0f, 1.0f,
+            -0.5f, -0.366f, 0.0f, 1.0f,
         ];
 
-        // we need to create a VAO to store the other buffers
+        // Create a VAO to store the other buffers
         glGenVertexArrays(1, &m_Vao);
         glBindVertexArray(m_Vao);
 
-        // this is the VBO that holds the vertex data
+        // VBO that holds the vertex data
         GLuint buffer;
         glGenBuffers(1, &buffer);
         glBindBuffer(GL_ARRAY_BUFFER, buffer);
         glBufferData(GL_ARRAY_BUFFER, vertex_data.length * float.sizeof,
                 vertex_data.ptr, GL_STATIC_DRAW);
 
-        // reset the state; we will re-enable the VAO when needed
+        // Reset the state; we will re-enable the VAO when needed
         glBindBuffer(GL_ARRAY_BUFFER, 0);
     }
 }
-
-void initShaders(uint* program_out, uint* mvp_location_out,
-        uint* position_location_out,
-        uint* color_location_out)
-{
-    const vertex = compileShader(GL_VERTEX_SHADER,
-            VertShaderCode ~ "\0");
-    scope (exit)
-        glDeleteShader(vertex);
-
-    const fragment = compileShader(GL_FRAGMENT_SHADER,
-            FragShaderCode ~ "\0");
-    scope (exit)
-        glDeleteShader(fragment);
-
-    const program = glCreateProgram();
-
-    glAttachShader(program, vertex);
-    scope (exit)
-        glDetachShader(program, vertex);
-
-    glAttachShader(program, fragment);
-    scope (exit)
-        glDetachShader(program, fragment);
-
-    glLinkProgram(program);
-
-    int status = 0;
-    glGetProgramiv(program, GL_LINK_STATUS, &status);
-
-    if (status == GL_FALSE)
-    {
-        int len = 0;
-        glGetProgramiv(program, GL_INFO_LOG_LENGTH, &len);
-
-        char[] buffer;
-        buffer.length = len + 1;
-        glGetProgramInfoLog(program, len, null, buffer.ptr);
-
-        glDeleteProgram(program);
-
-        throw new Exception(format("Linking failure in program: %s",
-                buffer));
-    }
-
-    *program_out = program;
-    *mvp_location_out = glGetUniformLocation(program, "mvp");
-    *position_location_out = glGetAttribLocation(program,
-            "position");
-    *color_location_out = glGetAttribLocation(program,
-            "color");
-}
-
-uint compileShader(int type, string source)
-{
-    const shader = glCreateShader(type);
-    scope (failure)
-        glDeleteShader(shader);
-    const(char)* srcPtr = source.ptr;
-    glShaderSource(shader, 1, &srcPtr, null);
-    glCompileShader(shader);
-
-    int status;
-    glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
-
-    if (status == GL_FALSE)
-    {
-        int len;
-        glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &len);
-
-        char[] buffer;
-        buffer.length = len + 1;
-        glGetShaderInfoLog(shader, len, null, buffer.ptr);
-
-        const sType = type == GL_VERTEX_SHADER ? "vertex"
-            : "fragment";
-
-        throw new Exception(format("Compilation failure in %s shader: %s",
-                sType, buffer));
-    }
-
-    return shader;
-}
-
-float[16] getIdentityMatrix() pure
-{
-    float[4 * 4] mat;
-
-    // identity matrix
-    for (int x = 0; x < 4; ++x)
-        for (int y = 0; y < 4; ++y)
-            mat[x + y * 4] = x == y ? 1 : 0;
-
-    return mat;
-}
-
-immutable FragShaderCode = import("fragment.glsl");
-immutable VertShaderCode = import("vertex.glsl");
---
+++ source/ui/boardgl/shaders.d
@@ -0,0 +1,81 @@
+module ui.boardgl.shaders;
+
+import glcore;
+import std.format;
+
+immutable FragShaderCode = import("fragment.glsl");
+immutable VertShaderCode = import("vertex.glsl");
+
+uint compileShader(int type, string source) {
+    const shader = glCreateShader(type);
+    scope (failure)
+        glDeleteShader(shader);
+    const(char)* srcPtr = source.ptr;
+    glShaderSource(shader, 1, &srcPtr, null);
+    glCompileShader(shader);
+
+    int status;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
+
+    if (status == GL_FALSE)
+    {
+        int len;
+        glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &len);
+
+        char[] buffer;
+        buffer.length = len + 1;
+        glGetShaderInfoLog(shader, len, null, buffer.ptr);
+
+        const sType = type == GL_VERTEX_SHADER ? "vertex"
+            : "fragment";
+
+        throw new Exception(format("Compilation failure in %s shader: %s",
+                sType, buffer));
+    }
+
+    return shader;
+}
+
+void initShaders(uint* program_out, uint* mvp_location_out,
+        uint* position_location_out, uint* color_location_out)
+{
+    const vertex = compileShader(GL_VERTEX_SHADER,
+            VertShaderCode ~ "\0");
+    scope (exit) glDeleteShader(vertex);
+
+    const fragment = compileShader(GL_FRAGMENT_SHADER,
+            FragShaderCode ~ "\0");
+    scope (exit) glDeleteShader(fragment);
+
+    const program = glCreateProgram();
+
+    glAttachShader(program, vertex);
+    scope (exit) glDetachShader(program, vertex);
+
+    glAttachShader(program, fragment);
+    scope (exit) glDetachShader(program, fragment);
+
+    glLinkProgram(program);
+
+    int status = 0;
+    glGetProgramiv(program, GL_LINK_STATUS, &status);
+
+    if (status == GL_FALSE) {
+        int len = 0;
+        glGetProgramiv(program, GL_INFO_LOG_LENGTH, &len);
+
+        char[] buffer;
+        buffer.length = len + 1;
+        glGetProgramInfoLog(program, len, null, buffer.ptr);
+
+        glDeleteProgram(program);
+
+        throw new Exception(format("Linking failure in program: %s",
+                buffer));
+    }
+
+    *program_out = program;
+    *mvp_location_out = glGetUniformLocation(program, "mvp");
+    *position_location_out = glGetAttribLocation(program, "position");
+    *color_location_out = glGetAttribLocation(program, "color");
+}
