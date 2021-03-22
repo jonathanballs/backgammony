64d33f0957650ee9265462179f8e92326830bb58
Jonathan Balls <jonathanballs@protonmail.com>
2021-03-22 18:33:35 +0000

    Remove commented out greyscale code from aa shader

--- source/ui/boardgl/shaders/antialiasing/fragment.glsl
+++ source/ui/boardgl/shaders/antialiasing/fragment.glsl
@@ -8,6 +8,5 @@ uniform sampler2D screenTexture;
 void main()
 {
     vec3 col = texture(screenTexture, TexCoords).rgb;
-    // float grayscale = 0.2126 * col.r + 0.7152 * col.g + 0.0722 * col.b;
     FragColor = vec4(col, 1.0);
 } 
